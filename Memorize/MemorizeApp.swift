ae2e9ee50d8cbe725d71fd5bdbdb87f526db85ab
Adrián Zúñiga <jaime.ziga@gmail.com>
2022-09-21 07:14:04 -0500

    First commit?

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jaime Zúñiga on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
