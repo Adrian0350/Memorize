73b1e536cea001067ffe7d2f702672128e6b5058
Adrián Zúñiga <jaime.ziga@gmail.com>
2022-09-21 23:10:04 -0500

    ZStack function, arguments and object properties | inheritance.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,58 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Adrián!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, Adrián!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
