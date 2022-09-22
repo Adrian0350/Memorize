96baf9574f692a7504dc9cdb707b7c2e53591790
Adrián Zúñiga <jaime.ziga@gmail.com>
2022-09-21 23:41:49 -0500

    Lecture 2: Local variables | Global Variables - var variables and let constants.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,18 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, Adrián!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
     }
 }
 
+struct CardView: View {
+    var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp
+            {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("🤖")
+                    .font(.largeTitle)
+            }
+            else
+            {
+                shape.fill()
+            }
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.light)
+        ContentView().preferredColorScheme(.dark)
     }
 }
