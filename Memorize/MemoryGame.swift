8cfb8800a20fd148257e875cd18b35d93073034d
Adrián Zúñiga <jaime.ziga@gmail.com>
2022-09-28 08:13:29 -0500

    Lecture 4: Memorize Game Logic - Introduction to Optionals, unnamed arguments in loops and Equatable behaviour for structs. (Game is now functional)

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -7,11 +7,12 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
+    private var indexOfTheOneAndOnlyFaceUpCard: Int?
 
     struct Card: Identifiable {
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
@@ -21,7 +22,8 @@ struct MemoryGame<CardContent> {
         // Add numberOfPairsOfCards x 2 to cards array
         cards = Array<Card>()
         
-        for pairIndex in 0..<numberOfPairsOfCards {
+        for pairIndex in 0..<numberOfPairsOfCards
+        {
             let content: CardContent = createCardContent(pairIndex)
 
             cards.append(Card(content: content, id: pairIndex * 2))
@@ -30,19 +32,32 @@ struct MemoryGame<CardContent> {
     }
     
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
         
-        cards[chosenIndex].isFaceUp.toggle()
-        print("\(cards)")
-    }
-    
-    func index(of card: Card) -> Int {
-        for index in 0..<cards.count {
-            if cards[index].id == card.id {
-                return index
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),  !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard
+            {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content
+                {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                
+                indexOfTheOneAndOnlyFaceUpCard = nil
             }
-        }
+            else
+            {
+                for index in cards.indices
+                {
+                    cards[index].isFaceUp = false
+                }
 
-        return 0
+                indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+            }
+            
+            cards[chosenIndex].isFaceUp.toggle()
+        }
+        
+        print("\(cards)")
     }
 }
